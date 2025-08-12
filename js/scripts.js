53d41a31de37048beb37b823b6646b6b687be202
Brian Bauska <brianbauska@gmail.com>
2025-08-12 12:16:13 -0700

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+/* js/scripts.js of CSS3-Ref making CSS3-ref.bauska.org */
+window.addEventListener('DOMContentLoaded', () => {
+
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			const id = entry.target.getAttribute('id');
+			if (entry.intersectionRatio > 0) {
+				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
+			} else {
+				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+			}
+		});
+	});
+
+	// Track all sections that have an `id` applied
+	document.querySelectorAll('section[id]').forEach((section) => {
+		observer.observe(section);
+	});
+	
+});
